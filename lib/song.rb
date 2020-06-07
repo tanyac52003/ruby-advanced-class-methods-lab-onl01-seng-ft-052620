cd5ae85b34d9b6ae3c48d9c11f0b559b87fd35cc
Tanya Hodges <tanyac52003@gmail.com>
2020-06-07 18:47:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,10 +36,27 @@ class Song
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end 
-    
   
-
-
+  def self.alphabetical
+    @@all.sort_by{|x| x.name}
+  end 
+  
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split"-")[1].chomp(".mp3"))
+    song.artist_name = (name.split("-")[0])
+    song
+  end
+  
+  def self.create_from_filename(name)
+    self.new_from_filename(name)
+    @@all << song
+    song 
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
 
 end
 
