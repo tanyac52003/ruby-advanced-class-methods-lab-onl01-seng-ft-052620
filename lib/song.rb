dc0fcf1c66f7b1dda2a7c08b220931f4644fc0fa
Tanya Hodges <tanyac52003@gmail.com>
2020-06-07 18:36:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,9 @@ class Song
   end 
   
   def self.new_by_name(name)
-    song = self.new_by_name(name)
-    song.name = name 
+    song = self.new
+    song.name = name  
+    song 
   end 
 
 end
