198f326b42f5c7e7bcdc4c300697090e0e729a0b
Tanya Hodges <tanyac52003@gmail.com>
2020-06-07 18:39:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,5 +21,29 @@ class Song
     song.name = name  
     song 
   end 
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    @@all << song
+    song 
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find {|x| x.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    
+  end 
+    
+  
+
+
 
 end
+
+
+
+
+
